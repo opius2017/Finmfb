e7d7146bdb3693e11d050543ba2be25139d9bc6a
kataracyber04-cpu <kataracyber04@gmail.com>
2025-09-23 05:37:30 +0100

    Refactor project files and update assembly information for FinTech.Tests and FinTech.WebAPI.

--- Fin-Backend.Tests/obj/Debug/net8.0/Fin-Backend.Tests.AssemblyInfo.cs
+++ Fin-Backend.Tests/obj/Debug/net8.0/Fin-Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fin-Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe2c2c6e00484fcf9dce538a8cf942ddc939583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c7a24601f7f20c1cd5b3a850a42924c9092ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fin-Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fin-Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
